eb988e217ac02185240a26ff99d76a4d1025cffa
weldson01 <weldsoncarlos007@gmail.com>
2024-07-08 00:18:57 -0300

    chore: linearList game map

--- src/app/(auth)/home/components/LinearList.tsx
+++ src/app/(auth)/home/components/LinearList.tsx
@@ -8,7 +8,7 @@ export default function LinearList({ title }: { title: string }) {
     return <div className="mt-10 pl-5">
         <h2 className="text-3xl text-whiteColor">{title}:</h2>
         <div className="flex py-10 px-10 w-full gap-5 overflow-x-scroll no-scrollbar">
-            {games.map(({ src,title, info,id }: IGame) => {
+            {games.map(({src,title, info,id }) => {
                 return <div className="" key={id}>
                     <Link href={`/game/${id}`}>
                         <Image width={860} height={520} src={src} alt={info} className="max-w-32 h-40 antialiased" />
